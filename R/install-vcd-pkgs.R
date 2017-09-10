d5e94d794d4986e1a6a3742e930c3e0c70f16977
Michael Friendly <friendly@yorku.ca>
2017-09-10 11:37:32 -0400

    update install-vcd-pkgs.R

--- R/install-vcd-pkgs.R
+++ R/install-vcd-pkgs.R
@@ -1,11 +1,12 @@
 # script to install needed lab packages for R Windows
 
-#  1. Open Rgui on the local drive or image server (e.g.,  drive:\R\R-3.1.1\bin\Rgui.exe)
-#  2. Run the following script
-#     - Select any convenient CRAN mirror from the pop-up list
+#  1. Open Rgui on the local drive or image server (e.g.,  drive:\R\R-3.4.1\bin\Rgui.exe)
 
-#  All packages should be installed into the library directory of the current R version
-#  (e.g., drive:\R\R-3.1.1\library)
+#  2a. Run the following script (e.g., copy/paste contents of this file to the R console)
+
+#  2b. Alternatively, just run the following line in the R console:
+#  source("http://euclid.psych.yorku.ca/www/psy6136/R/install-vcd-pkgs.R")
+#   
 
 # main packages for the VCD short course
 vcdpkgs <-c("vcdExtra", "ca", "car", "catspec", "coin", "corrgram", "effects", 
@@ -13,7 +14,8 @@ vcdpkgs <-c("vcdExtra", "ca", "car", "catspec", "coin", "corrgram", "effects",
 
 # other useful packages
 otherpkgs <-c("ade4", "candisc", "ggplot2", "heplots", "HH", "rms", "latticeExtra", 
-"lme4", "plyr", "Rcmdr", "RcmdrPlugin.HH", "reshape", "rgl", "sem", "TeachingDemos")
+"lme4", "dplyr", "Rcmdr", "RcmdrPlugin.HH", "reshape", "rgl", "sem", "TeachingDemos",
+"tidyverse", "knitr", "psych", "lavaan" )
 
 allpkgs <- union(vcdpkgs, otherpkgs)
 
